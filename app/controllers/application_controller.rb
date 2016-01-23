5c4cf6dd409cdd259e8240df5b70460f207b2d1f
JonShoenig <Jonsho25@gmail.com>
2016-01-23 14:44:23 -0500

    created orders controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
+	
+	def index
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def destroy
+	end
+	
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
