c91db604bb6aa37bd7ef062c5a18aa66136f1bc2
JonShoenig <Jonsho25@gmail.com>
2016-02-01 21:22:49 -0500

    updated for exercise 4.6

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,15 @@
 class StaticPagesController < ApplicationController
   def index
+  	@products = Product.all
   end
   def landing_page
   	@featured_product = Product.last
+  	@products = Product.all
+  end
+  def contact
+  	@products = Product.all
+  end
+  def about
   	@products = Product.all
   end
 end
