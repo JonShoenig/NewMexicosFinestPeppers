d4afe62dc2992602307e77992664faa492df1faf
JonShoenig <Jonsho25@gmail.com>
2016-02-09 11:38:57 -0500

    updated contact page with email

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,4 +12,13 @@ class StaticPagesController < ApplicationController
   def about
   	@products = Product.all
   end
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+      :to => 'your-email@example.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+  end 
 end
