cbb6c460a5b6c25c4c2ea4ae7597a8e2ceb25449
JonShoenig <Jonsho25@gmail.com>
2016-03-11 10:37:43 -0500

    rspec

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '4.2.5'
 # Use sqlite3 as the database for Active Record
 group :development, :test do
 	gem 'sqlite3'
+	gem 'rspec-rails', '~> 3.0'
 end
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe Product do
+	context "when the product has comments" do
+		before do
+			@product = Product.create!(name: "ghost pepper")
+			@user = User.create!(email: "email@domain.com", password: "abcd1234")
+			@product.comments.create! (rating: 3, user: @user, body: "Too spicy for me!")
+			@product.comments.create! (rating: 5, user: @user, body: "Loved this pepper! I only recommend it if you love spicy food")
+			@product.comments.create! (rating: 1, user: @user, body: "Set my mouth on fire for days")
+		end
+	
+
+		it "returns the average rating of all comments" do
+		expect(@product.average_rating).to eq 3
+		end
+	end 
+end 
