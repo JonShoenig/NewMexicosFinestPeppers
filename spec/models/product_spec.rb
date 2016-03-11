f605c7d8fa55eae92cb07e19da15f3d8108d262d
JonShoenig <Jonsho25@gmail.com>
2016-03-11 11:23:23 -0500

    added rspec unit test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,14 +5,17 @@ describe Product do
 		before do
 			@product = Product.create!(name: "ghost pepper")
 			@user = User.create!(email: "email@domain.com", password: "abcd1234")
-			@product.comments.create! (rating: 3, user: @user, body: "Too spicy for me!")
-			@product.comments.create! (rating: 5, user: @user, body: "Loved this pepper! I only recommend it if you love spicy food")
-			@product.comments.create! (rating: 1, user: @user, body: "Set my mouth on fire for days")
+			@product.comments.create!(rating: 3, user: @user, body: "Too spicy for me!")
+			@product.comments.create!(rating: 5, user: @user, body: "Loved this pepper! I only recommend it if you love spicy food")
+			@product.comments.create!(rating: 1, user: @user, body: "Set my mouth on fire for days")
 		end
 	
-
 		it "returns the average rating of all comments" do
 		expect(@product.average_rating).to eq 3
+		end
+
+		it "is not valid" do
+		expect (@product.new(description: "extremely spicy!")).not_to be_valid	
 		end
 	end 
 end 
