dea830097801b66f20ee01206b979da122c5f8b2
JonShoenig <Jonsho25@gmail.com>
2016-04-08 17:34:42 -0400

    Angular 3

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,9 +1,9 @@
 var app = angular.module('shop', ['ngResource']);
-
+//Workaround to get Angular to work
 $(document).on('ready page:load', function() {
 	angular.bootstrap(document.body, ['shop'])
 });
-
+//factory for models
 app.factory('models', ['$resource', function($resource){
 	var orders_model = $resource("/orders/:id.json", {id: "@id"});
 	var products_model = $resource("/products/:id.json", {id: "@id"});
@@ -13,12 +13,13 @@ app.factory('models', ['$resource', function($resource){
 	};
 	return x;
 }]);
-
+//connect app to OrdersCtrl controller
 app.controller('OrdersCtrl', ['$scope', 'models', function($scope, models){
 	$scope.orders = models.orders.query();
 	$scope.products = models.products.query();
-
+//add new orders
 	$scope.addOrder = function(){
+		//only orders with product id or total zero get added
 		if(!$scope.newOrder.product_id || $scope.newOrder.total === ''){ return; } //Order can be submitted if productid and total are filled out
 		order = models.orders.save($scope.newOrder, function(){ //POST
 			recent_order = models.orders.get({id: order.id});
@@ -26,7 +27,7 @@ app.controller('OrdersCtrl', ['$scope', 'models', function($scope, models){
 			$scope.newOrder = '';
 		});
 	}
-
+//delete orders
 	$scope.deleteOrder = function(order){
 		models.orders.delete(order);
 		$scope.orders.splice($scope.orders.indexOf(order), 1);
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -34,6 +34,6 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:product_id, :user_id, :total)
+    params.require(:order).permit(:product_id, :user_id, :total, :email)
   end
 end
